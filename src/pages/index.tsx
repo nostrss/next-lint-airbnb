dac5d271c53ac39037a857f5f1c0076fb54431fa
nostrss <jintagi@gmail.com>
2023-03-18 00:09:19 +0900

    add testing lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div>hi</div>
+        <h1>hi</h1>
       </main>
     </>
   );
